17e9bc5e8aea0101caef6ae592b95d3e1b0f13f4
Abdul <awsayed@alaska.edu>
2019-05-01 23:22:06 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,6 +6,11 @@ birthday_kids = {
 
 def happy_birthday(birthday_kids)
   
+  birthday_kids.each { |kids_name, age}
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  
+  }
+  
 end
 
 
